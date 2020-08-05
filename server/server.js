4d34502dbbafe046ee47afdbc4e26aa98a1b9aee
samlak <samlak1999@gmail.com>
2020-08-05 01:54:26 +0100

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
-require('./config/config');
+require('./config/default');
+if(process.env.STAGE === "Development") {
+    require('./config/config');
+}
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
