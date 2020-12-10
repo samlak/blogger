663001af7c32ac3fcd37b54fa2d472f924de2194
samlak <samlak1999@gmail.com>
2020-12-11 00:51:43 +0100

    Remove config file

--- server/config/config.js
+++
@@ -1,3 +0,0 @@
-process.env.PORT = 3100;
-process.env.MONGODB_URI = "mongodb+srv://samlak:Odun!999@cluster0.8hdco.mongodb.net/Blogger";
-process.env.JWT_SECRET = "dsdsddsgjhdsyewyuw783264gsdsdnbu4";
