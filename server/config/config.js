64013fea40e451a4ff500b66fd49828dfd0dea26
samlak <samlak1999@gmail.com>
2020-12-10 22:42:01 +0100

    database url changed

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,3 @@
 process.env.PORT = 3100;
-process.env.MONGODB_URI = "mongodb://localhost:27017/Blogger";
+process.env.MONGODB_URI = "mongodb+srv://samlak:Odun!999@cluster0.8hdco.mongodb.net/Blogger";
 process.env.JWT_SECRET = "dsdsddsgjhdsyewyuw783264gsdsdnbu4";
